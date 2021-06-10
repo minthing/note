4b0f0a9b98ec82872de870ce81133ddad853e144
minthing <minjeongandk@gmail.com>
2021-06-10 12:09:51 +0900

    router 설치

--- vue-default/src/main.js
+++ vue-default/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app') //index id="app"인 곳에 연결시켜라
+createApp(App).use(router).mount('#app') //index id="app"인 곳에 연결시켜라
