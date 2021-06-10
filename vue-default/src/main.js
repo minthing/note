0af157331150ff89eaf89a162e74b106cbb5d536
minthing <minjeongandk@gmail.com>
2021-06-10 12:19:50 +0900

    router 구조 실습

--- vue-default/src/main.js
+++ vue-default/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router' // router 폴더를 가리킴 => 라우터 안의 index.js가 라우터에 자동으로 들어감
 
-createApp(App).use(router).mount('#app') //index id="app"인 곳에 연결시켜라
+const app = createApp(App);
+app.use(router);
+app.mount('#app') //index id="app"인 곳에 연결시켜라
+// use(router)
